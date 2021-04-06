36eb64c08048bbe0a0ce15a33c284ed89df99c2a
saideep9835 <tsaideep98@msitprogram.net>
2021-04-06 17:21:57 +0530

    third commit

--- lru-cache.py
+++ lru-cache.py
@@ -3,3 +3,9 @@ class LRUcache:
     def __init__(self, capacity : int):
         self.cache = OrderedDict()
         self.capacity = capacity
+    def get(self, key : int) -> int:
+        pass
+    def put(self, key: int, value: int) -> None:
+        pass
+    def get_cache(self):
+        pass
